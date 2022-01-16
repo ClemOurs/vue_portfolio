28cdc1e0bc20555cff8ce5f1081ebcb7491fb4bf
Clément <duvivier.clem@gmail.com>
2022-01-16 22:51:02 +0100

    :white_check_mark: contact page done

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,44 +1,53 @@
 <template>
-    <div class="contact-header column">
-        <h1 class="gradient">Contact me.</h1>
-        <p>Do not hesitate to contact me, I will answer you as soon as possible !</p>
-    </div>
-    <div class="infos flex-wrap">
-        <div class="reach column">
-            <div class="container-info contact">
-                <h5>by email.</h5>
-                <div class="email button row center-total">
-                    <img src="@/assets/logos/mail.png" alt="mail">
-                    <p>duvivier.clem@gmail.com</p>
-                </div>
-            </div>
-            <div class="container-info contact">
-                <h5>by phone.</h5>
-                <div class="row center-total">
-                    <img src="@/assets/logos/phone.png" alt="phone">
-                    <p>+33.6.83.70.18.85</p>
-                </div>
-            </div>
+    <div class="contact-page">
+        <div class="contact-header column">
+            <h1 class="gradient">Contact me.</h1>
+            <p>Do not hesitate to contact me, I will answer you as soon as possible !</p>
         </div>
-        <div class="container-info-socials contact">
-            <h5>by socials.</h5>
-            <div class="row">
-                <img src="@/assets/logos/linkedin-fill.png" alt="social">
-                <p>Clément Duvivier</p>
+        <div class="infos flex-wrap">
+            <div class="reach column">
+                <div class="container-info contact">
+                    <h5>by email.</h5>
+                    <div class="email row info">
+                        <img src="@/assets/logos/mail.png" alt="mail">
+                        <p class="gradient">duvivier.clem@gmail.com</p>
+                    </div>
+                </div>
+                <div class="container-info contact">
+                    <h5>by phone.</h5>
+                    <div class="row info">
+                        <img src="@/assets/logos/phone.png" alt="phone">
+                        <p class="gradient">+33.6.83.70.18.85</p>
+                    </div>
+                </div>
             </div>
-            <div class="row">
-                <img src="@/assets/logos/github.png" alt="social">
-                <p>ClemOurs</p>
+            <div class="container-info-socials contact">
+                <h5>by socials.</h5>
+                <div class="row">
+                    <img src="@/assets/logos/linkedin-fill.png" alt="social">
+                    <a class="gradient" href="https://www.linkedin.com/in/clementduvivier/">Clément Duvivier</a>
+                </div>
+                <div class="row">
+                    <img src="@/assets/logos/github.png" alt="social">
+                    <a class="gradient" href="https://github.com/ClemOurs">ClemOurs</a>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <style lang="scss">
+    .contact-page {
+        height: 100vh;
+    }
     .contact-header {
         display: flex;
+        gap: 20px;
         align-items: center;
-        margin-bottom: 50px;
+        margin: 70px;
+        p {
+            text-align: center;
+        }
     }
     .infos {
         justify-content: center;
@@ -49,7 +58,12 @@
         }
     }
     .reach {
+        width: 300px;
+        min-height: 100px;
         gap: 21px;
+    }
+    .info {
+        align-items: center;
     }
     .container-info {
         padding: 15px;
